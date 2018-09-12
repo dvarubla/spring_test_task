ac9dda66006e7602cb8d1588e113204df19125a3
dvarubla <dvarubla@users.noreply.github.com>
2018-09-12 21:05:47 +0700

    Validate purchase num

--- src/main/java/ru/spring/testtask/MainController.java
+++ src/main/java/ru/spring/testtask/MainController.java
@@ -2,42 +2,55 @@ package ru.spring.testtask;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.spring.testtask.dao.PurchaseRepository;
 
+import javax.validation.Valid;
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
-@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
 @Controller
 @RequestMapping("/")
 public class MainController {
     private PurchaseRepository _purchaseRepository;
+    private UIntValidator _uIntValidator;
 
-    MainController(PurchaseRepository purchaseRepository){
+    MainController(PurchaseRepository purchaseRepository, UIntValidator validator){
         _purchaseRepository = purchaseRepository;
+        _uIntValidator = validator;
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.setValidator(_uIntValidator);
     }
 
     @GetMapping
-    public String getRoot(Optional<Integer> num, Model model){
-        if(num.isPresent()) {
-            final BigDecimal[] totalPurchase = {new BigDecimal(0)};
-            List<PurchaseInfo> purchaseList =
-                    _purchaseRepository.findByNum(num.get())
-                            .stream().map(p -> {
-                                BigDecimal total = p.getProduct().getPrice().multiply(BigDecimal.valueOf(p.getCount()));
-                                totalPurchase[0] = totalPurchase[0].add(total);
-                                return new PurchaseInfo(
-                                    p.getProduct().getName(), p.getCount(), p.getProduct().getPrice(), total
-                                );
-                            }
-                    ).collect(Collectors.toList());
-            model.addAttribute("purchaseNum", num.get());
-            model.addAttribute("total", totalPurchase);
-            model.addAttribute("purchases", purchaseList);
+    public String getRoot(@ModelAttribute("num") @Valid String numStr, BindingResult bindingResult, Model model){
+        if(numStr.length() != 0) {
+            if(!bindingResult.hasErrors()) {
+                int num = Integer.valueOf(numStr);
+                final BigDecimal[] totalPurchase = {new BigDecimal(0)};
+                List<PurchaseInfo> purchaseList =
+                        _purchaseRepository.findByNum(num)
+                                .stream().map(p -> {
+                                    BigDecimal total = p.getProduct().getPrice().multiply(BigDecimal.valueOf(p.getCount()));
+                                    totalPurchase[0] = totalPurchase[0].add(total);
+                                    return new PurchaseInfo(
+                                            p.getProduct().getName(), p.getCount(), p.getProduct().getPrice(), total
+                                    );
+                                }
+                        ).collect(Collectors.toList());
+                model.addAttribute("purchaseNum", num);
+                model.addAttribute("total", totalPurchase);
+                model.addAttribute("purchases", purchaseList);
+            }
         }
         return "index.html";
     }
---
+++ src/main/java/ru/spring/testtask/UIntValidator.java
@@ -0,0 +1,29 @@
+package ru.spring.testtask;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+class UIntValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.equals(String.class);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        String str = (String) target;
+        if(str.length() == 0){
+            return;
+        }
+        try{
+            int num = Integer.valueOf(str);
+            if(num < 0){
+                errors.rejectValue(null, "negativeNumber", "Вы ввели отрицательное число");
+            }
+        } catch (Exception e){
+            errors.rejectValue(null,"notANumber", "Вы ввели не число");
+        }
+    }
+}
