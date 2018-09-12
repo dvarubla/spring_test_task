44247a578f67b0649e9f54478ac071b97b0c2db5
dvarubla <dvarubla@users.noreply.github.com>
2018-09-12 17:26:04 +0700

    Correct CSS serving

--- src/main/java/ru/spring/testtask/config/Thymeleaf.java
+++ src/main/java/ru/spring/testtask/config/Thymeleaf.java
@@ -2,13 +2,15 @@ package ru.spring.testtask.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @SuppressWarnings("SpringFacetCodeInspection")
 @Configuration
-public class Thymeleaf {
+public class Thymeleaf implements WebMvcConfigurer {
     @Bean
     public SpringTemplateEngine templateEngine() {
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
@@ -32,5 +34,10 @@ public class Thymeleaf {
         viewResolver.setCharacterEncoding("UTF-8");
         viewResolver.setTemplateEngine(templateEngine());
         return viewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
     }
 }
